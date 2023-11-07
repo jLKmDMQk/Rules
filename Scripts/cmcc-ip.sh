84bd42104904935d7c0dd43e1a7edc8d3ea0bc25
jLKmDMQk <looney.mitosis-0f@icloud.com>
2023-11-08 01:45:07 +0800

    update

--- Scripts/cmcc-ip.sh
+++ Scripts/cmcc-ip.sh
@@ -3,11 +3,17 @@ export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~
 
 source ./Scripts/base.sh
 
-url="https://ispip.clang.cn/cmcc_cidr.txt"
+wget -q -O $domain_raw_file "https://ispip.clang.cn/cmcc_cidr.txt"
+cat $domain_raw_file >>$domain_raw_list_file
+echo "" >>$domain_raw_list_file
 
-wget -q -O $domain_raw_file $url
+wget -q -O $domain_raw_file "https://ispip.clang.cn/crtc_cidr.txt"
+cat $domain_raw_file >>$domain_raw_list_file
+echo "" >>$domain_raw_list_file
+
+cat $domain_raw_list_file | grep -v "^[[:space:]]*$" >>$domain_list_file
 
 # Generate RouterOS RSC
 echo "/ip firewall address-list remove [/ip firewall address-list find list=cmcc-ip-list]" >./routeros/cmcc-ip-list.rsc
 echo "/ip firewall address-list" >>./routeros/cmcc-ip-list.rsc
-cat $domain_raw_file | awk '{print "add address="$0" disabled=no list=cmcc-ip-list"}' >>./routeros/cmcc-ip-list.rsc
+cat $domain_list_file | awk '{print "add address="$0" disabled=no list=cmcc-ip-list"}' >>./routeros/cmcc-ip-list.rsc
