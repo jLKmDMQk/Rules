bb5efa0e61847bb325c823d022215275af8e2328
jLKmDMQk <looney.mitosis-0f@icloud.com>
2024-03-02 22:51:03 +0800

    Added cryptocurrency rule set and URLs for UK and US media

--- Scripts/Overseas/config.sh
+++ Scripts/Overseas/config.sh
@@ -53,8 +53,12 @@ geosite_filenames=(
 )
 
 rule_set_urls=(
+    "rule/Surge/Cryptocurrency/Cryptocurrency.list"
     "rule/Surge/UKMedia/UKMedia.list"
     "rule/Surge/USMedia/USMedia.list"
+
+    "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/Porn.list"
+    "https://raw.githubusercontent.com/LM-Firefly/Rules/master/PROXY/Porn.list"
 )
 
 domain_list_urls=(
