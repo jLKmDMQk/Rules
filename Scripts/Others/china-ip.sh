a0668a9d9a924ce764554ef3318bb68f10a04b97
jLKmDMQk <looney.mitosis-0f@icloud.com>
2024-03-02 18:49:27 +0800

    update

--- Scripts/Others/china-ip.sh
+++ Scripts/Others/china-ip.sh
@@ -7,16 +7,16 @@ url="https://github.com/Loyalsoldier/surge-rules/raw/release/cncidr.txt"
 
 wget -q -O $domain_raw_file $url
 
-cat $domain_raw_file | awk -F',' '{print $2}' >$domain_raw_list_file
+cat "$domain_raw_file" | awk -F',' '{print $2}' >$domain_raw_list_file
 
 cp $domain_raw_list_file ./list/china-ip.list
 
-cp $domain_raw_file ./surge/china-ip-ruleset.list
+cp "$domain_raw_file" ./surge/china-ip-ruleset.list
 
-cat $domain_raw_list_file | awk '{print "whitelist-ip "$0}' >./smartdns/whitelist-ip.conf
+cat "$domain_raw_list_file" | awk '{print "whitelist-ip "$0}' >./smartdns/whitelist-ip.conf
 
 
 # Generate RouterOS RSC
 echo "/ip firewall address-list remove [/ip firewall address-list find list=china-ip-list]" >./routeros/china-ip-list.rsc
 echo "/ip firewall address-list" >> ./routeros/china-ip-list.rsc
-cat $domain_raw_list_file | awk '{print "add address="$0" disabled=no list=china-ip-list"}' >> ./routeros/china-ip-list.rsc
+cat "$domain_raw_list_file" | awk '{print "add address="$0" disabled=no list=china-ip-list"}' >> ./routeros/china-ip-list.rsc
