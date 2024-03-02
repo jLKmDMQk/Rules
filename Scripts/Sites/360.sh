50a2cc82adc5eb8f0bad13bed3f6064598881b85
jLKmDMQk <looney.mitosis-0f@icloud.com>
2024-03-02 18:30:37 +0800

    update

--- Scripts/Sites/360.sh
+++ Scripts/Sites/360.sh
@@ -4,12 +4,8 @@ export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~
 source ./Scripts/Utils/base.sh
 source ./Scripts/Utils/common.sh
 
-rule_set_urls=(
-    "Clash/360/360.list"
-)
-
 geosite_filenames=(
-    "360@all"
+    "qihoo360"
 )
 
 sites_common_handle "./surge/qihoo-domain.list" "./surge/qihoo-ruleset.list" "./list/qihoo-site.list"
