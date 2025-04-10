a34f93db43542205c2c5609b1c1ee99d9405bf98
jLKmDMQk <looney.mitosis-0f@icloud.com>
2025-04-10 09:54:53 +0800

    refactor: 更新脚本以支持Clash配置文件生成
    修改alibaba.sh和tencent.sh脚本，添加Clash配置文件的生成路径。删除不再使用的domain.list文件。

--- Scripts/Sites/alibaba.sh
+++ Scripts/Sites/alibaba.sh
@@ -12,4 +12,4 @@ geosite_filenames=(
     "alibaba"
 )
 
-sites_common_handle "./result/surge/alibaba-domain.list" "./result/surge/alibaba-ruleset.list" "./result/list/ali-site.list"
+sites_common_handle "./result/surge/alibaba-domain.list" "./result/surge/alibaba-ruleset.list" "./result/list/ali-site.list" "" "./result/clash/alibaba-domain.yaml"
