5b82ba0250656aecafab7a54df19dd939b692275
jLKmDMQk <looney.mitosis-0f@icloud.com>
2025-04-10 09:35:52 +0800

    chore: 在geosite_filenames中添加category-dev@!cn

--- Scripts/Overseas/config.sh
+++ Scripts/Overseas/config.sh
@@ -23,6 +23,7 @@ geosite_filenames=(
     "category-browser-!cn@!cn"
     "category-cas@!cn"
     "category-cryptocurrency@!cn"
+    "category-dev@!cn"
     "category-ecommerce@!cn"
     "category-entertainment@!cn"
     "category-ipfs@!cn"
